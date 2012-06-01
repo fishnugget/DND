43d4a07ca96e96734b0ec72b624fe62ad74d4fab
Jonathan James <jonathan@schoolofthelegends.com>
2012-06-01 15:57:24 -0500

    Make splash page work on line

---
+++ app/Controller/EmailsController.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Static content controller.
+ *
+ * This file will render views from views/pages/
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Controller
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+
+/**
+ * Static content controller
+ *
+ * Override this controller by placing a copy in controllers directory of an application
+ *
+ * @package       app.Controller
+ * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
+ */
+class SignupsController extends AppController {
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	public $name = 'Signups';
+
+/**
+ * Default helper
+ *
+ * @var array
+ */
+	public $helpers = array('Html', 'Session');
+
+/**
+ * This controller does not use a model
+ *
+ * @var array
+ */
+	public $uses = array('Signup');
+
+/**
+ * Displays a view
+ *
+ * @param mixed What page to display
+ * @return void
+ */
+	public function index() {
+		
+	}
+	
+	public function splash(){
+		debug($this->request->data);
+		exit();
+			
+	}
+}
